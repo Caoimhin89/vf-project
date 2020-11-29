8fc988f4ba88a4b0ff39d2f67104cb01d55ac6d8
kevin-glick <k.glick@smart-code.team>
2020-11-29 10:10:42 -0700

    add test for get key from putObject event

--- __tests__/handler.test.js
+++ __tests__/handler.test.js
@@ -1,6 +1,14 @@
+const { test, expect } = require('@jest/globals');
 const handler = require('../handler');
+const testEvents = require('../test-events');
 
 test('hello, world', async () => {
   const response = await handler.hello();
   expect(JSON.parse(response.body).message).toBe("Hello, world!");
+});
+
+test('retrieve key from S3 PutObject', async () => {
+  const { s3PutObject } = testEvents;
+  const response = await handler.uploadParser(s3PutObject);
+  expect(JSON.parse(response.body).message.objectKey).toBe("test/key");
 });
